58c05bfd044184e621f974dd13ab0eb5aaef2806
Dulea Mihai <duleasoft@gmail.com>
2023-03-12 17:05:29 +0200

    added the Entry page

---
+++ BlackHoleChess/BlackHoleChess/EntryPage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackHoleChess
+{
+    internal class EntryPage
+    {
+        private Label label = new Label();
+        private ComboBox comboBox = new ComboBox();
+        private Form activeForm = Form.ActiveForm;
+
+        public EntryPage() {
+            //Label
+            label.Text = "BLACK HOLE CHESS";
+            label.Location = new Point(activeForm.Width / 2, 200);
+
+
+            //Drop Box
+            comboBox.Location = new Point(activeForm.Width / 2, 400);
+            comboBox.BackColor = System.Drawing.Color.Orange;
+            comboBox.ForeColor = System.Drawing.Color.Black;
+            comboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox.Items.Add("Black");
+            comboBox.Items.Add("White");
+        }
+
+        public void showEntryPage()
+        {
+            activeForm.Controls.Add(label);
+            activeForm.Controls.Add(comboBox);
+        }
+        
+        public void hideEntryPage()
+        {
+            activeForm.Controls.Remove(label);
+            activeForm.Controls.Remove(comboBox);
+        }
+
+        public string getDropBoxChoice()
+        {
+            return comboBox.Text;
+        }
+    }
+}
